580b2382e26402b5c8a84fcd1bfc6959e7ca407b
Rkhan2@rrc.ca <Rkhan2@rrc.ca>
2025-09-26 12:30:32 +0530

    chore: add jest config for testing

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/*.test.ts'],
+};
+
+export default config;
